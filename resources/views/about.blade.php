f492eb5772f66bc64a35dddd72f51356da366a0b
ruotkun <ruotproject@gmail.com>
2023-12-20 11:46:46 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,118 @@
+<!doctype html>
+
+
+<html lang="en">
+
+
+<head>
+
+
+   <meta charset="utf-8" />
+
+
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+       crossorigin="anonymous" />
+
+
+   <title>Online Store-about</title>
+
+   <style>
+.bg-secondary{
+    background-color: darkgreen !important;
+}
+</style>
+
+</head>
+
+
+
+<body>
+
+
+   <!-- header -->
+
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+
+
+       <div class="container">
+
+
+           <a class="navbar-brand" href="#">Online Store</a>
+
+
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+               aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+
+               <span class="navbar-toggler-icon"></span>
+
+
+           </button>
+
+
+           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+
+               <div class="navbar-nav ms-auto">
+
+
+                   <a class="nav-link active" href="#">Home</a>
+
+
+                   <a class="nav-link active" href="#">Products</a>
+
+
+                   <a class="nav-link active" href="#">Cart</a>
+
+
+                   <a class="nav-link active" href="index">Index</a>
+
+
+               </div>
+
+
+           </div>
+
+
+       </div>
+
+
+   </nav>
+
+
+   <header class="masthead bg-primary text-white text-center py-4">
+
+
+       <div class="container d-flex align-items-center flex-column">
+
+
+           <h2>About the Online Store</h2>
+
+
+       </div>
+
+
+   </header>
+
+
+   <!-- header -->
+
+
+   <div class="container my-4">
+
+
+   </div>
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+   </script>
+
+
+</body>
+
+
+</html>
